d53408262fafce6d78eaf6fa21eaad05865fff96
livier92 <livihh@hotmail.com>
2018-03-07 23:47:49 -0600

    Add structure to admin routes

--- server.js
+++ server.js
@@ -2,42 +2,15 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var adminRoutes = require('./routes/admin');
 
 // send our index.html file to the user for the home page
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-// create routes for the admin section
-
-// get an instance of the router
-var adminRouter = express.Router();
-
-// route middleware that will happen on every request
-adminRouter.use(function(req, res, next) {
-	//log each request to the console
-	console.log(req.method, req.url);
-	//continue doing what we were doing and go to the route
-	next();
-});
-
-// admin main page, the dashboard (http://localhost:3000/admin)
-adminRouter.get('/', function(req, res) {
-	res.send('I am the dashborad!');
-});
-
-// users page (http://localhost:3000/users)
-adminRouter.get('/users', function(req, res) {
-	res.send('I show all the users!');
-});
-
-// posts page (http://localhost:3000/posts)
-adminRouter.get('/posts', function(req, res) {
-	res.send('I show all the posts!');
-});
-
 // apply the routes to our application
-app.use('/admin', adminRouter);
+app.use('/admin', adminRoutes);
 
 // start the server
 app.listen(3000);
