e7e61be618212d62ce5dec58776459d9d4523a79
livier92 <livihh@hotmail.com>
2018-03-08 23:34:50 -0600

    setting up the server

--- server.js
+++ server.js
@@ -1,13 +1,54 @@
-//load the express package and create our app
+// BASE SETUP
+// ======================================
+
+// CALL THE PACKAGES --------------------
 var express = require('express');
 var app = express();
-var path = require('path');
+var bodyParser = require('body-parser'); // get body-parser
+var morgan = require('morgan'); // used to see requests
+var mongoose = require('mongoose'); // for working w/ our database
+var port = process.env.PORT || 3000; // set the port for our app
 
-// send our index.html file to the user for the home page
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
+// APP CONFIGURATION ---------------------
+// use body parser so we can grab information from POST requests
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// configure our app to handle CORS requests
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \Authorization');
+	next();
 });
 
-// start the server
-app.listen(3000);
-console.log('3000 is the magic port!');
+// log all requests to the console
+app.use(morgan('dev'));
+
+// ROUTES FOR OUR API
+// =============================
+
+// basic route for the home page
+app.get('/', function(req, res) {
+	res.send('Welcome to the home page!');
+});
+
+// get an instance of the express router
+var apiRouter = express.Router();
+
+// test route to make sure everything is working
+// accessed at GET http://localhost:8080/api
+apiRouter.get('/', function(req, res) {
+	res.json({ message: 'hooray! welcome to our api!' });
+});
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+ app.use('/api', apiRouter);
+
+// START THE SERVER
+// ===============================
+app.listen(port);
+console.log('Magic happens on port ' + port);
