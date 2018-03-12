90389c736c35bd491783df4e8bcf1f1fbe22a9bd
livier92 <livihh@hotmail.com>
2018-03-12 15:11:00 -0600

    Set up the server express and project structure

--- server.js
+++ server.js
@@ -3,11 +3,14 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+// set the public folder to serve public assets
+app.use(express.static(__dirname + '/public'));
+
 // send our index.html file to the user for the home page
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/index.html'));
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/views/index.html'));
 });
 
-// start the server
+// start the server (http://localhost:3000)
 app.listen(3000);
 console.log('3000 is the magic port!');
