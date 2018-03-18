0e43c8ddf0012c94258aebff81b88a05c4bae85e
livier92 <livihh@hotmail.com>
2018-03-18 14:51:20 -0600

    Restructure backend file

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'port': process.env.PORT || 8080, // set the port for our app
+	'database': 'mongodb://node:noder@novus.modulusmongo.net:27017/Iganiq8o',
+	'secret': 'ilovescotchscotchyscotchscotch'
+ };
--- server.js
+++ server.js
@@ -7,14 +7,7 @@ var app = express();
 var bodyParser = require('body-parser'); // get body-parser
 var morgan = require('morgan'); // used to see requests
 var mongoose = require('mongoose'); // for working w/ our database
-var port = process.env.PORT || 3000; // set the port for our app
-var apiRoutes = require('./routes/api');
-
-// connect to our database (hosted on mongoLab)
-mongoose.connect('mongodb://localhost:27017/db_name');
-mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+var config = require('./config');
 
 // APP CONFIGURATION ---------------------
 // use body parser so we can grab information from POST requests
@@ -32,16 +25,32 @@ app.use(function(req, res, next) {
 // log all requests to the console
 app.use(morgan('dev'));
 
-// basic route for the home page
-app.get('/', function(req, res) {
-	res.send('Welcome to the home page!');
-});
+// connect to our database
+mongoose.connect(config.database);
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 // REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
- app.use('/api', apiRoutes);
+// set static files location
+// used for requests that our frontend will make
+app.use(express.static(__dirname + '/public'));
+
+// ROUTES FOR OUR API =================
+// ====================================
+
+// API ROUTES ------------------------
+var apiRoutes = require('./app/routes/api')(app, express);
+app.use('/api', apiRoutes);
+
+// MAIN CATCHALL ROUTE ---------------
+// SEND USERS TO FRONTEND ------------
+// has to be registered after API ROUTES
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/app/views/index.html'));
+});
 
 // START THE SERVER
 // ===============================
-app.listen(port);
-console.log('Magic happens on port ' + port);
+app.listen(config.port);
+console.log('Magic happens on port ' + config.port);
